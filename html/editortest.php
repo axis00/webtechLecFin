cb594b902986c3eb3800b1bbc5534c3c5538749b
David Paul Brackin <dev.mintgames@gmail.com>
2018-04-27 21:16:11 +0800

    added phpeditor include and deleted unnessecary files from the ace library

---
+++ html/editortest.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../styles/editorstyles.css">
+        <style>
+            body{
+                margin: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+            include '../includes/phpeditor.html';
+        ?>
+    </body>
+</html>
