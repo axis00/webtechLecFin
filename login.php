a450a9b19e0cd04f1ffddde7ea819edbfec71062
gabozkhi <gabrielbucsit5@gmail.com>
2018-05-12 11:54:48 +0800

    Added login and register to nav bar and styled their correspending php files

--- login.php
+++ login.php
@@ -38,13 +38,65 @@
 
 ?>
 
-<!DOCTYPE html>
-<html>
-	<body>
-		<form action="login.php" method="POST">
-			<input type="text" name="username" placeholder="Username">
-			<input type="password" name="password" placeholder="Password">
-			<input type="submit" value="login">
-		</form>
-	</body>
-</html>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="../styles/style.css">
+        <link rel="stylesheet" type="text/css" href="../styles/nav.css">
+        <link rel="stylesheet" type="text/css" href="/styles/login.css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+        <script src="../scripts/jquery-3.2.1.min.js"></script>
+        <title>Login</title>
+    </head>
+
+    <body>
+        <nav>
+            <ul>
+                <li><a href="./index.html">Home</a></li>
+                <li>
+                    <div class="dropdown">
+                        <div class="dropdown-button"><a href="./html/finals.html">Server Side Scripting</a>
+                            <div class="dropdown-content">
+                                <a href="./html/java.html">Java</a>
+                                <a href="./html/php.html">PHP</a>
+                                <a href="./html/javascript.html">JavaScript</a>
+                                <a href="./html/websecurity.html">Web Security</a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="dropdown">
+                        <div class="dropdown-button">
+                            <a href="./html/flashcards/index.php">Review</a>
+                        </div>
+                    </div>
+                </li>
+                <li><a href="./html/glossaryv2.html">Glossary</a></li>
+                <li><a href="./html/about.html">About</a></li>
+                <li><a href="./login.php">Login</a></li>
+                <li><a href="./register.php">Register</a></li>
+            </ul>
+        </nav>
+        <div id="mini-hero-banner">
+            <div id="particles-js"></div>
+            <script type="text/javascript" src="../scripts/particles.js"></script>
+            <script type="text/javascript" src="../scripts/particles-app.js"></script>
+            <div class="wrapper">
+                <h1>Login</h1>
+            </div>
+        </div>
+        <div id="login-container">
+            <form action="login.php" method="POST">
+                <p>Username</p>
+                <input type="text" name="username">
+                <br>
+                <p>Password</p>
+                <input type="password" name="password">
+                <br><input type="submit" value="LOGIN">
+            </form>
+        </div>
+    </body>
+
+    </html>
